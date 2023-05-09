9b1c9279612b796edb1c16bee55507152bccfecd
TinLe1310 <tintrungle1310@gmail.com>
2023-05-09 11:40:57 -0500

    Polish Final Product Page.

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,15 @@ import Link from "next/link";
 import style from "./button.module.css";
 
 function Button(props){
-    return(
-        <Link href={props.link} className={style.btn}>
-            {props.children}
-        </Link>
-    );
+    if(props.link){
+        return(
+            <Link href={props.link} className={style.btn}>
+                {props.children}
+            </Link>
+        ); 
+    }
+
+    return <button className={style.btn} onClick={props.onClick}>{props.children}</button>;
 }
 
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,23 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
 function AllEvents(){
+    const events = getAllEvents();
+    const router = useRouter(); 
+
+    function findEvent(year, month){
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
     return (
-        <div>
-            <h1>All Events</h1>
-        </div>
+        <Fragment>
+            <EventSearch onSearch={findEvent}/>
+            <EventList items = {events}/>
+        </Fragment>
     );
 }
 
